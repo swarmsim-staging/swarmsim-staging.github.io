b18bebca6cae0314fb2988ecafeef2e1c73c96aa
Evan <github@erosson.org>
2017-05-31 23:39:00 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -6,4 +6,11 @@ self.addEventListener('install', () => {
   // new service worker is activated immediately, even if there's
   // another tab open controlled by our older service worker code.
   self.skipWaiting();
+
+  // Clear the cache from that broken version.
+  caches.keys().then(function (cachesNames) {
+    return Promise.all(cachesNames.map(function (cacheName) {
+      return caches.delete(cacheName)//.then(function () {
+    }))
+  });
 });
